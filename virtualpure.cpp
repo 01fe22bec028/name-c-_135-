3502ce84cd25a0c00c9649b29c9c052e982249c5
01fe22bec028 <130641749+01fe22bec028@users.noreply.github.com>
2026-04-27 11:50:59 +0530

    Add files via upload

---
+++ virtualpure.cpp
@@ -0,0 +1,23 @@
+#include<iostream.h>
+using namespace std;
+class shape
+{
+protected :
+     float x;
+public:
+    void getdata(){cin>>x;}
+    0;
+};
+class square : public shape
+{
+public :
+    float calculateArea()
+    { return x*x ;}
+
+
+};
+class circle : public shape
+{
+public :
+    float calculateArea
+};
