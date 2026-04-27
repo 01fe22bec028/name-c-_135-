02737ee15e2cc3c0b23fe4f56731d4e2699d3df8
01fe22bec028 <130641749+01fe22bec028@users.noreply.github.com>
2026-04-27 11:58:58 +0530

    Add files via upload

---
+++ prog2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int a, b;   // fixed declaration
+
+    cout << "Enter the first number to add: " << endl;
+    cin >> a;
+
+    cout << "Enter the second number to add: " << endl;
+    cin >> b;
+
+    cout << "Result = " << a + b << endl;
+
+    return 0;   // fixed return statement
+}
